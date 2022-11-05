a0acaff08d9e00d4a8279d436f7ca0597c8e1a55
MustafaBaig51 <baigmustafa47@gmail.com>
2022-11-05 10:52:44 +0500

    minor fixes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
-// Main Routes
+// Main Routess
 app.use('/books', BookRoute);
 
 // Undefined Routes
--- routes/Book.route.js
+++ routes/Book.route.js
@@ -10,7 +10,7 @@ router.get("/get-books", async (req, res) => {
     res.status(200).json(books);
 });
 
-router.get("/search-books", async (req, res) => {
+router.post("/search-books", async (req, res) => {
     const regex = new RegExp(req.body.searchText, 'i');
     const books = await Book.find({ title: regex });
     res.status(200).json(books);
